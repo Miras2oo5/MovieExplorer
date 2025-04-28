edf969b3fc2b1f18bda11b76a7e9458a3c967997
Miras <mirasorazbaj321@gmail.com>
2025-04-29 02:06:39 +0600

    login

--- app/src/main/java/com/example/movieexplorer/api/ApiInterface.java
+++ app/src/main/java/com/example/movieexplorer/api/ApiInterface.java
@@ -1,4 +1,23 @@
 package com.example.movieexplorer.api;
 
-public class ApiInterface {
+import com.example.movieexplorer.model.Movie;
+import com.example.movieexplorer.model.MovieResponse;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+
+public interface ApiInterface {
+    @GET("movie/popular")
+    Call<MovieResponse> getPopularMovies(
+            @Query("api_key") String apiKey,
+            @Query("page") int page
+    );
+
+    @GET("movie/{movie_id}")
+    Call<Movie> getMovieDetails(
+            @Path("movie_id") int movieId,
+            @Query("api_key") String apiKey
+    );
 }
